9c8ab389354d197b5308754fee5a9ca3379f17eb
Sara <sarakirving@gmail.com>
2020-08-05 19:33:23 -0700

    Update to use response.data instead of response

--- src/components/Application.js
+++ src/components/Application.js
@@ -90,7 +90,7 @@ export default function Application(props) {
 
   useEffect(() => {
     axios.get('/api/days')
-    .then(response => setDays(response));
+    .then((response) => {setDays(response.data)});
   }, [])
 
   const appointmentsList = appointments.map(appointment => {
