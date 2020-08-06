c5ef8528fd9e9788a41cde2d119b0d3acddd3dce
Sara <sarakirving@gmail.com>
2020-08-06 13:22:45 -0700

    transform data from API before it is passed in as props to the Appointment component

--- src/components/Application.js
+++ src/components/Application.js
@@ -87,7 +87,8 @@ export default function Application(props) {
 
   const [state, setState] = useState({day: "Monday",
                                     days: [],
-                                    appointments: {}
+                                    appointments: {},
+                                    interviewers: {}
                                   });
 
   const setDay = day => setState({ ...state, day });
@@ -100,18 +101,15 @@ export default function Application(props) {
 
     Promise.all([
       Promise.resolve(axios.get('/api/days')),
-      Promise.resolve(axios.get('/api/appointments')) 
+      Promise.resolve(axios.get('/api/appointments')),
+      Promise.resolve(axios.get('/api/interviewers'))
     ])
     .then((all) => {
-      // let [days, appointments] = all;
-      // console.log(days);
-      // console.log(appointments);
-      console.log(all[0]);
+      console.log(all[2].data);
       setState(prev => ({...prev, days: all[0].data,
-                          appointments: all[1].data}))
+                          appointments: all[1].data,
+                          interviewers: all[2].data}))
     });
-
-
 
     //axios.get('/api/days')
     //.then((response) => {setDays(response.data)});
@@ -120,13 +118,17 @@ export default function Application(props) {
   const appointments = getAppointmentsForDay(state, state.day)
 
   const appointmentsList = appointments.map(appointment => {
+    const interview = getInterview(state, appointment.interview);
+
     return (
       <Appointment 
       key={appointment.id}
-      {...appointment}
+      id={appointment.id}
+      time={appointment.time}
+      interview={interview}
       />
-    )
-  })
+    );
+  });
 
   return (
     <main className="layout">
