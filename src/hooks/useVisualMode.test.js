721d61051e2db5faa489bdb6ab9f13e688ef071a
Sara <sarakirving@gmail.com>
2020-08-06 15:34:46 -0700

    Add second test for transition property of useVisualMode

--- src/hooks/useVisualMode.test.js
+++ src/hooks/useVisualMode.test.js
@@ -2,11 +2,21 @@ import { renderHook, act } from "@testing-library/react-hooks";
 
 import useVisualMode from "hooks/useVisualMode";
 
-
+//test initializing the mode with a default value
 const FIRST = "FIRST";
 
 test("useVisualMode should initialize with default value", () => {
   const { result } = renderHook(() => useVisualMode(FIRST));
 
   expect(result.current.mode).toBe(FIRST);
+});
+
+// test transitioning from one mode to another 
+const SECOND = "SECOND";
+
+test("useVisualMode should transition to another mode", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  act(() => result.current.transition(SECOND));
+  expect(result.current.mode).toBe(SECOND);
 });
