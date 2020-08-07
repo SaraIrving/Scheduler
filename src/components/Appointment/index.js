ba6720d0c09524ec010a4aad231463dd17add16a
Sara <sarakirving@gmail.com>
2020-08-06 19:39:32 -0700

    updated props that we will pass to Form component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,16 +5,31 @@ import "components/Appointment/styles.scss";
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
+import Form from "components/Appointment/Form";
 
+import useVisualMode from "../../hooks/useVisualMode";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
+const SAVING = "SAVING";
 
 
 export default function Appointment (props) {
+
+  const {mode, transition, back} = useVisualMode(props.interview ? SHOW : EMPTY);
+
+  console.log('props inside Appointment component = ', props);
+    
   return (
     <article className="appointment">
       <Header 
       time={props.time}
       />
-      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty />}
+      {mode === EMPTY && <Empty onAdd={event => transition(CREATE)} />}
+      {mode === SHOW && <Show student={props.interview.student} interviewer={props.interview.interviewer} />}
+      {mode === CREATE && <Form interviewers={[]} onSave={event => transition(SAVING)} onCancel={event => back()} />}
+      
     </article>
   )
 }
