d9d606813b9b07168e2c04d29c1c58232245a200
Sara <sarakirving@gmail.com>
2020-08-11 09:02:55 -0700

    fixed spelling error in Monday

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -9,5 +9,5 @@ afterEach(cleanup);
 it("defaults to Monday and changes the schedule when a new day is selected", () => {
   const { getByText } = render(<Application />);
 
-  return waitForElement(() => getByText("MOnday"));
+  return waitForElement(() => getByText("Monday"));
 })
