795f191100ea4f6c3737d004c3b57690f13248fd
Sara <sarakirving@gmail.com>
2020-08-11 16:46:59 -0700

    add integration test to check that the Application component loads data, books an interview and reduces the spots remaining for the first day by 1

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent, getByText, prettyDOM } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, getByText, prettyDOM, getAllByTestId, getByPlaceholderText, getByAltText, waitForElementToBeRemoved, queryByText } from "@testing-library/react";
 
 import Application from "components/Application";
 
@@ -23,17 +23,41 @@ describe("Application", () => {
 
   it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
 
-    const { container } = render(<Application />);
+    const { container, debug } = render(<Application />);
 
     await waitForElement(() => getByText(container, "Archie Cohen"));
 
-    console.log(prettyDOM(container));
+    const appointment = getAllByTestId(container, "appointment")[0];
 
+
+    // simulate clicking on the add appointment icon
+    fireEvent.click(getByAltText(appointment, "Add"));
+
+    // simulate filling in name in the input field
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+
+    // simulate selecting an interviewer from the list of options by searching for the interviewers name with is the alternate text for the image, name comes from first interviewer in the list of fake data in mocks
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+
+    // simulate clicking the save button
+    fireEvent.click(getByText(appointment, "Save"));
+
+    expect(getByText(appointment, "Saving")).toBeInTheDocument();
+
+    await waitForElementToBeRemoved(() => getByText(appointment, "Saving"))
+
+    expect(getByText(appointment, /Lydia Miller-Jones/i)).toBeInTheDocument();
+
+    // get an array of the days, find the one that has the text "Monday"
+    const day = getAllByTestId(container, "day").find(day => queryByText(day, "Monday"));
+
+    expect(getByText(day, "no spots remaining")).toBeInTheDocument();
 
   });
 
-
-})
+});
 
 
 
