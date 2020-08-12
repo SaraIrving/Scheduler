7c8582898c9153c3210e4cdf6ebc046847da050f
Sara <sarakirving@gmail.com>
2020-08-11 19:27:53 -0700

    add a test to check that it 'loads data, cancels an interview and increases the spots remaining for Monday by 1', this test is having issues with data integrity, the it block that comes before it is altering it's starting data so that Monday has no spots available instead of the expected 1 spot available. This is causing the test to fail because we end up with 1 spot available instead of the expected 2. Since the test is failing because of this data integrity issue but is demonstrating that the code does in fact behave as we would like, I am leaving it as is in the hopes that I may be able to address the data integrity issue at a later date as I cannot solve it now and it has been suggested it is realted to the depreciation of some of the functions used in the test or the afterEach(cleanup) code

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,12 +1,16 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent, getByText, prettyDOM, getAllByTestId, getByPlaceholderText, getByAltText, waitForElementToBeRemoved, queryByText } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, getByText, prettyDOM, getAllByTestId, getByPlaceholderText, getByAltText, waitForElementToBeRemoved, queryByText, queryByAltText } from "@testing-library/react";
 
 import Application from "components/Application";
+
+import { act } from 'react-dom/test-utils';
 
 afterEach(cleanup);
 
 describe("Application", () => {
+
+  
 
   it("defaults to Monday and changes the schedule when a new day is selected", async () => {
     const { getByText } = render(<Application />);
@@ -56,6 +60,51 @@ describe("Application", () => {
     expect(getByText(day, "no spots remaining")).toBeInTheDocument();
 
   });
+
+  
+
+  it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
+    //1. Render the Application
+    const { container, debug } = render(<Application />);
+
+    //2. Wait until the text "Archie Cohen" is displayed
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+    debug();
+
+     //3. Click the "delete" button on the booked appointment
+    const appointment = getAllByTestId(container, "appointment").find(appointment => queryByText(appointment, "Archie Cohen"));
+
+
+    fireEvent.click(queryByAltText(appointment, "Delete"));
+ 
+
+    //4. Check that the confirmation message is shown
+    expect(getByText(appointment, "Are you sure you want to delete?")).toBeInTheDocument();
+    
+
+    //5. Click the "confirm" button on the confirmation
+    fireEvent.click(queryByText(appointment, "Confirm"));
+
+    //6. Check that the element with the text "Deleting" is displayed 
+    expect(getByText(appointment, "Deleting")).toBeInTheDocument();
+   
+
+    //7. Wait until the element with the "Add" button is displayed
+    await waitForElement(() => queryByAltText(appointment, "Add"));
+    
+
+    //8. Check that the DayListItem with the text "Monday" also has the text "2 spots remaining"
+    const day = getAllByTestId(container, "day").find(day => queryByText(day, "Monday"));
+
+
+    expect(getByText(day, "2 spots remaining")).toBeInTheDocument();
+
+
+    debug();
+
+
+
+  } )
 
 });
 
