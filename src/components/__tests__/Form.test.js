4365fb39b3fdaae20fe61e72de9a3a431a5124b9
Sara <sarakirving@gmail.com>
2020-08-11 19:23:30 -0700

    experimented with moving around afterEach(cleanup) to see if it fixed the data integrity issue I am having with my Application tests, it did not so I put it back in it's original space

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -4,12 +4,14 @@ import { render, cleanup, fireEvent } from "@testing-library/react";
 
 import Form from "components/Appointment/Form";
 
-afterEach(cleanup);
+//afterEach(cleanup);
 
 
 // tests related to the Form component
 
 describe("Form", () => {
+
+  
   //fake interviewers data
   const interviewers = [
     {
@@ -21,14 +23,19 @@ describe("Form", () => {
 
 
   it("renders without student name if not provided", () => {
-    const { getByPlaceholderText } = render(<Form  interviewers={interviewers} />); 
+    const { getByPlaceholderText } = render(
+      <Form  interviewers={interviewers} />
+      ); 
+
     expect(getByPlaceholderText("Enter Student Name")).toHaveValue("");
   });
+
 
   it("renders with initial student name", () => {
     const { getByTestId } = render(
       <Form interviewers={interviewers} name="Lydia Miller-Jones" />
-    );
+      );
+      
     expect(getByTestId('student-name-input')).toHaveValue("Lydia Miller-Jones");
   });
 
@@ -36,7 +43,9 @@ describe("Form", () => {
   it("validates that the student name is not blank", () => {
     const onSave = jest.fn();
 
-    const { getByText } = render(<Form interviewers={interviewers} onSave={onSave} name={""} />);
+    const { getByText } = render(
+      <Form interviewers={interviewers} onSave={onSave} name={""} />
+    );
 
     fireEvent.click(getByText("Save"));
 
