a60218b4f0adf7946df65e24916eab0d000c2784
Sara <sarakirving@gmail.com>
2020-08-15 17:30:18 -0700

    removed Promise.resolve statements wrapping the three axios requests in the Promise.all statement as they are unnecessary

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -17,12 +17,11 @@ export default function useApplicationData () {
   useEffect(() => {
 
     Promise.all([
-      Promise.resolve(axios.get('/api/days')),
-      Promise.resolve(axios.get('/api/appointments')),
-      Promise.resolve(axios.get('/api/interviewers'))
+      axios.get('/api/days'),
+      axios.get('/api/appointments'),
+      axios.get('/api/interviewers')
     ])
     .then((all) => {
-      console.log(all[2].data);
       setState(prev => ({...prev, days: all[0].data,
                           appointments: all[1].data,
                           interviewers: all[2].data}))
